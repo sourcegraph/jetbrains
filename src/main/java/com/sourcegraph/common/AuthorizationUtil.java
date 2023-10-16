f51bd15d01de6129f69a4a517f08c29ae67b29a5
Will Dollman <will.dollman@sourcegraph.com>
2023-10-16 14:36:34 +0100

    Update access token format

--- src/main/java/com/sourcegraph/common/AuthorizationUtil.java
+++ src/main/java/com/sourcegraph/common/AuthorizationUtil.java
@@ -6,6 +6,7 @@ public class AuthorizationUtil {
   public static boolean isValidAccessToken(@NotNull String accessToken) {
     return accessToken.isEmpty()
         || accessToken.length() == 40
-        || (accessToken.startsWith("sgp_") && accessToken.length() == 44);
+        || (accessToken.startsWith("sgp_") && accessToken.length() == 44)
+        || (accessToken.startsWith("sgp_") && ( accessToken.length() == 61) || accessToken.length() == 62)); // See https://docs.google.com/document/d/1aC4gHB8Q5lurwVhc8SCxznR0blNJMKo7yIkpP7WShno
   }
 }
