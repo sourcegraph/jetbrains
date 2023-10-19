19ae16a696750e2f813812acb3a4670e44ce9121
Will Dollman <will.dollman@sourcegraph.com>
2023-10-19 08:57:07 +0100

    Fix line length and add sgph_ prefix

--- src/main/java/com/sourcegraph/common/AuthorizationUtil.java
+++ src/main/java/com/sourcegraph/common/AuthorizationUtil.java
@@ -7,6 +7,8 @@ public class AuthorizationUtil {
     return accessToken.isEmpty()
         || accessToken.length() == 40
         || (accessToken.startsWith("sgp_") && accessToken.length() == 44)
-        || (accessToken.startsWith("sgp_") && (accessToken.length() == 61 || accessToken.length() == 62)); // See https://docs.google.com/document/d/1aC4gHB8Q5lurwVhc8SCxznR0blNJMKo7yIkpP7WShno
+        // See https://docs.google.com/document/d/1aC4gHB8Q5lurwVhc8SCxznR0blNJMKo7yIkpP7WShno
+        || (accessToken.startsWith("sgp_") && accessToken.length() == 61)
+        || (accessToken.startsWith("sgph_") && accessToken.length() == 62);
   }
 }
