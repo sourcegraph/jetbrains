ec9bec5dd164ae6713fce219e4193d8a033ccfdc
Will Dollman <will.dollman@sourcegraph.com>
2023-10-20 12:13:59 +0100

    gradlew :spotlessApply

--- src/main/java/com/sourcegraph/common/AuthorizationUtil.java
+++ src/main/java/com/sourcegraph/common/AuthorizationUtil.java
@@ -5,8 +5,6 @@ import org.jetbrains.annotations.NotNull;
 public class AuthorizationUtil {
   public static boolean isValidAccessToken(@NotNull String accessToken) {
     // Sourcegraph access token formats: https://docs.sourcegraph.com/dev/security/secret-formats
-    return accessToken.isEmpty()
-        || accessToken.length() == 40
-        || accessToken.startsWith("sgp_");
+    return accessToken.isEmpty() || accessToken.length() == 40 || accessToken.startsWith("sgp_");
   }
 }
