8ed814123628293e6e0143df8c1e0afdd75c9792
Will Dollman <will.dollman@sourcegraph.com>
2023-10-20 12:06:43 +0100

    Simplify token matching and add docs URL

--- src/main/java/com/sourcegraph/common/AuthorizationUtil.java
+++ src/main/java/com/sourcegraph/common/AuthorizationUtil.java
@@ -4,11 +4,9 @@ import org.jetbrains.annotations.NotNull;
 
 public class AuthorizationUtil {
   public static boolean isValidAccessToken(@NotNull String accessToken) {
+    // Sourcegraph access token formats: see https://docs.sourcegraph.com/dev/security/secret-formats
     return accessToken.isEmpty()
         || accessToken.length() == 40
-        || (accessToken.startsWith("sgp_") && accessToken.length() == 44)
-        // See https://docs.google.com/document/d/1aC4gHB8Q5lurwVhc8SCxznR0blNJMKo7yIkpP7WShno
-        || (accessToken.startsWith("sgp_") && accessToken.length() == 61)
-        || (accessToken.startsWith("sgph_") && accessToken.length() == 62);
+        || accessToken.startsWith("sgp_");
   }
 }
