6dfb1b600b78843a3e4c1ed164ccc24ea4216b9f
Will Dollman <will.dollman@sourcegraph.com>
2023-10-20 12:13:17 +0100

    Tweak comment

--- src/main/java/com/sourcegraph/common/AuthorizationUtil.java
+++ src/main/java/com/sourcegraph/common/AuthorizationUtil.java
@@ -4,7 +4,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class AuthorizationUtil {
   public static boolean isValidAccessToken(@NotNull String accessToken) {
-    // Sourcegraph access token formats: see https://docs.sourcegraph.com/dev/security/secret-formats
+    // Sourcegraph access token formats: https://docs.sourcegraph.com/dev/security/secret-formats
     return accessToken.isEmpty()
         || accessToken.length() == 40
         || accessToken.startsWith("sgp_");
