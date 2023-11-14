583394034645ead0fcc1834f93580bf4d876f93c
Mikołaj Kondratek <mik.kondratek@gmail.com>
2023-11-14 15:41:12 +0100

    Add Cody status action related to the RateLimitError - add warning dialogs

--- src/main/kotlin/com/sourcegraph/cody/statusbar/ChatAndAutocompleteLimitWarningAction.kt
+++ src/main/kotlin/com/sourcegraph/cody/statusbar/ChatAndAutocompleteLimitWarningAction.kt
@@ -2,13 +2,20 @@ package com.sourcegraph.cody.statusbar
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
-import com.sourcegraph.cody.config.CodyApplicationSettings
+import com.intellij.openapi.ui.Messages
 import com.sourcegraph.config.ConfigUtil
 
 class ChatAndAutocompleteLimitWarningAction :
     DumbAwareAction("<html><b>Warning:</b> Chat and Autocomplete Limit Reached...</html>") {
   override fun actionPerformed(e: AnActionEvent) {
-    CodyApplicationSettings.instance.isCodyAutocompleteEnabled = true
+    val result: Int =
+        Messages.showDialog(
+            e.project,
+            "You've used all messages and autocompletions. The allowed number of request per day is limited at the moment to ensure the service stays functional.",
+            "Chat and Autocomplete Limit Reached",
+            arrayOf("Ok"),
+            /* defaultOptionIndex= */ 0,
+            Messages.getWarningIcon())
   }
 
   override fun update(e: AnActionEvent) {
