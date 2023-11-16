3ac2fe6e954d7d9bcd26928e33c534db2e80915d
Mikołaj Kondratek <mik.kondratek@gmail.com>
2023-11-16 20:41:20 +0100

    Paint `Insert at Cursor` and `Copy` buttons with the same `DarculaButtonUI` as the other buttons

--- src/main/kotlin/com/sourcegraph/cody/ui/TransparentButton.kt
+++ src/main/kotlin/com/sourcegraph/cody/ui/TransparentButton.kt
@@ -1,17 +1,12 @@
 package com.sourcegraph.cody.ui
 
-import java.awt.AlphaComposite
-import java.awt.BasicStroke
-import java.awt.Color
-import java.awt.Dimension
-import java.awt.Graphics
-import java.awt.Graphics2D
+import java.awt.*
 import javax.swing.JButton
 
 class TransparentButton(text: String, private val textColor: Color) : JButton(text) {
   private val opacity = 0.7f
   private val cornerRadius = 5
-  private val horizontalPadding = 10
+  private val horizontalPadding = 15
   private val verticalPadding = 5
 
   init {
@@ -28,6 +23,7 @@ class TransparentButton(text: String, private val textColor: Color) : JButton(te
 
   override fun paintComponent(g: Graphics) {
     val g2 = g.create() as Graphics2D
+    ui.update(g2, this)
     g2.composite = AlphaComposite.SrcOver.derive(opacity)
     g2.color = background
     g2.fillRoundRect(0, 0, width, height, cornerRadius, cornerRadius)
@@ -36,14 +32,5 @@ class TransparentButton(text: String, private val textColor: Color) : JButton(te
     g2.drawRoundRect(0, 0, width - 1, height - 1, cornerRadius, cornerRadius)
     g2.dispose()
     g.color = textColor
-    val fm = g.fontMetrics
-    val rect = fm.getStringBounds(text, g)
-    val textHeight = rect.height.toInt()
-    val textWidth = rect.width.toInt()
-
-    // Center text horizontally and vertically
-    val x = (width - textWidth) / 2
-    val y = (height - textHeight) / 2 + fm.ascent
-    g.drawString(text, x, y)
   }
 }
