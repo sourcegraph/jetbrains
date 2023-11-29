6b5689f101850619392e7706c820461730957ba0
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2023-11-29 16:11:24 +0000

    Migrate from filePaths to URIs

--- src/main/kotlin/com/sourcegraph/cody/agent/protocol/AutocompleteParams.kt
+++ src/main/kotlin/com/sourcegraph/cody/agent/protocol/AutocompleteParams.kt
@@ -1,13 +1,22 @@
 package com.sourcegraph.cody.agent.protocol
 
+import java.net.URI
+
 enum class AutocompleteTriggerKind(val value: String) {
   AUTOMATIC("Automatic"),
   INVOKE("Invoke"),
 }
 
 data class AutocompleteParams(
-    val filePath: String,
+    val uri: URI,
     val position: Position,
     val triggerKind: String? = AutocompleteTriggerKind.AUTOMATIC.value,
     val selectedCompletionInfo: SelectedCompletionInfo? = null
-)
+) {
+  constructor(
+      filePath: String,
+      position: Position,
+      triggerKind: String? = AutocompleteTriggerKind.AUTOMATIC.value,
+      selectedCompletionInfo: SelectedCompletionInfo? = null
+  ) : this(URI("file://$filePath"), position, triggerKind, selectedCompletionInfo)
+}
--- src/main/kotlin/com/sourcegraph/cody/agent/protocol/TextDocument.kt
+++ src/main/kotlin/com/sourcegraph/cody/agent/protocol/TextDocument.kt
@@ -1,11 +1,20 @@
 package com.sourcegraph.cody.agent.protocol
 
+import java.net.URI
+
 data class TextDocument
 // JvmOverloads needed until CodyAgentFocusListener
 // and CodyFileEditorListener are converted to Kotlin.
 @JvmOverloads
 constructor(
-    var filePath: String,
+    var uri: URI,
     var content: String? = null,
     var selection: Range? = null,
-)
+) {
+  @JvmOverloads
+  constructor(
+      filePath: String,
+      content: String? = null,
+      selection: Range? = null,
+  ) : this(URI("file://$filePath"), content, selection)
+}
