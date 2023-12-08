48bee87750115c9eaae2000ebf5f442dfa23fe05
spawlak <spawlak@virtuslab.com>
2023-12-08 17:21:08 +0100

    Adding endpoints to call agent for feature flag and user pro status

--- src/main/java/com/sourcegraph/cody/SubscriptionPanel.kt
+++ src/main/java/com/sourcegraph/cody/SubscriptionPanel.kt
@@ -1,22 +1,19 @@
 package com.sourcegraph.cody
 
 import com.intellij.ide.BrowserUtil
-import com.intellij.openapi.project.Project
+import com.intellij.ide.ui.laf.darcula.ui.DarculaButtonUI
 import com.intellij.ui.dsl.builder.panel
-import com.sourcegraph.cody.agent.CodyAgent
 import com.sourcegraph.config.ConfigUtil
 
-fun createSubscriptionTab(project: Project) = panel {
-  row { label("<html>Current tier: <b>Cody Pro</b><html/>") }
+fun createSubscriptionTab(isCurrentUserPro: Boolean) = panel {
+  val tier = if (isCurrentUserPro) "Cody Pro" else "Cody Free"
+  row { label("<html>Current tier: <b>$tier</b><html/>") }
   row {
-    button("Upgrade") { BrowserUtil.browse(ConfigUtil.DOTCOM_URL + "cody/subscription") }
-
-    if (CodyAgent.getServer(project) != null) {
-      val server = CodyAgent.getServer(project)
-      val evaluateFeatureFlag = server!!.evaluateFeatureFlag("cody-pro")
-      if (evaluateFeatureFlag) {
-        button("Check Usage") { BrowserUtil.browse(ConfigUtil.DOTCOM_URL + "cody/manage") }
-      }
+    if (!isCurrentUserPro) {
+      val upgradeButton =
+          button("Upgrade") { BrowserUtil.browse(ConfigUtil.DOTCOM_URL + "cody/subscription") }
+      upgradeButton.component.putClientProperty(DarculaButtonUI.DEFAULT_STYLE_KEY, true)
     }
+    button("Check Usage") { BrowserUtil.browse(ConfigUtil.DOTCOM_URL + "cody/manage") }
   }
 }
--- src/main/kotlin/com/sourcegraph/cody/config/test.java
+++
@@ -1,8 +0,0 @@
-package com.sourcegraph.cody.config;
-
-import javax.swing.*;
-
-public class test {
-  private JButton button1;
-  private JButton button2;
-}
