8e32730c70314bfa408bfab040480d65b289f1f3
Olafur Geirsson <olafurpg@gmail.com>
2023-12-14 15:04:35 +0100

    Agent: move from recipes to the new chat UI
    Previously, the JetBrains plugin used the `chat-question` recipe to power chat.
    This was problematic because this recipe was not used by VS Code meaning
    that users had a different experience in JetBrains and VS Code, usually
    a worse experience in JetBrains because it didn't keep up with the rapid
    context improvements in VS Code.
    
    Now, with this PR, JetBrains uses new `chat/*` and `webview/*` related
    endpoints in the agent to power chat. This PR requires the changes from
    https://github.com/sourcegraph/cody/pull/2396 to be merged. The new chat
    UI in VS Code is entirely designed around webviews that have a low-level
    notification-based protocol (no requests). This low-lever protocol is a
    bit more complicated to work with but it supports richer functionality,
    including triggering indexing of local embeddings, manually adding
    individual files to the context, selecting the language model, and more.
    This PR only implements the basics: submitting a message, stopping a
    reply, and handling rate limit errors.
    
    Importantly, this PR alone won't close the full gap against VS Code for
    chat or commands. This is just an important milestone towards that goal.

--- build.gradle.kts
+++ build.gradle.kts
@@ -232,6 +232,10 @@ tasks {
     }
     val codyDir = unzipCody()
     println("Using cody from codyDir=$codyDir")
+    if (System.getenv("CODY_DIR") != null) {
+      // Use `pnpm agent` instead
+      return buildCodyDir
+    }
     exec {
       workingDir(codyDir)
       commandLine("pnpm", "install", "--frozen-lockfile")
@@ -381,6 +385,7 @@ tasks {
     // Specify pre-release label to publish the plugin in a custom Release Channel automatically.
     // Read more:
     // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+
     channels.set(
         listOf(
             properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
---
+++ src/main/kotlin/com/sourcegraph/cody/webview/CodyWebviewPanel.kt
@@ -0,0 +1,3 @@
+package com.sourcegraph.cody.webview
+
+class CodyWebviewPanel(id: String) {}
