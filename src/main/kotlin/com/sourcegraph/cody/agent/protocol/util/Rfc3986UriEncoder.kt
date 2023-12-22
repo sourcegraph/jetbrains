4bdae5c0c7d72f2f75678736fa6349213472e152
Jakub Blach <jakubblach@gmail.com>
2023-12-22 12:19:45 +0100

    support lowercase partition letters

--- src/main/kotlin/com/sourcegraph/cody/agent/protocol/util/Rfc3986UriEncoder.kt
+++ src/main/kotlin/com/sourcegraph/cody/agent/protocol/util/Rfc3986UriEncoder.kt
@@ -4,9 +4,9 @@ object Rfc3986UriEncoder {
 
   // todo solve this with library
   fun encode(uri: String): String {
-    val isWindowsPath = uri.matches("^file:///[A-Z]:/.*".toRegex())
+    val isWindowsPath = uri.matches("^file:///[A-Za-z]:.*".toRegex())
     if (isWindowsPath) {
-      val found = "file:///([A-Z]):".toRegex().find(uri)!!
+      val found = "file:///([A-Za-z]):".toRegex().find(uri)!!
       val partition = found.groups[1]!!.value
       return uri.replace("file:///$partition:", "file:///${partition.lowercase()}%3A")
     }
--- src/test/kotlin/com/sourcegraph/cody/agent/protocol/util/Rfc3986UriEncoderTest.kt
+++ src/test/kotlin/com/sourcegraph/cody/agent/protocol/util/Rfc3986UriEncoderTest.kt
@@ -9,6 +9,11 @@ class Rfc3986UriEncoderTest : TestCase() {
     assertEquals("file:///c%3A/Users/user/Test.java", fixedUri)
   }
 
+  fun `test encode Windows path with lowercase partition`() {
+    val fixedUri = Rfc3986UriEncoder.encode("file:///c:/Users/user/Test.java")
+    assertEquals("file:///c%3A/Users/user/Test.java", fixedUri)
+  }
+
   fun `test encode Linux path`() {
     val uri = Rfc3986UriEncoder.encode("file:///home/user/Test.java")
     assertEquals("file:///home/user/Test.java", uri)
