8997d2c62f5a6bec47075592e5e49205be98b8f7
spawlak <spawlak@virtuslab.com>
2024-01-10 15:34:17 +0100

    Fixing separator and addressing github comments

--- src/main/java/com/sourcegraph/cody/SubscriptionPanel.kt
+++ src/main/java/com/sourcegraph/cody/SubscriptionPanel.kt
@@ -6,28 +6,48 @@ import com.intellij.ui.dsl.builder.panel
 import com.sourcegraph.common.CodyBundle
 import com.sourcegraph.common.UpgradeToCodyProNotification
 import com.sourcegraph.config.ConfigUtil
+import com.sourcegraph.config.ThemeUtil
 
 fun createSubscriptionTab(isCurrentUserPro: Boolean) = panel {
   val chatLimitError = UpgradeToCodyProNotification.chatRateLimitError.get()
   val autocompleteLimitError = UpgradeToCodyProNotification.autocompleteRateLimitError.get()
+  val chatLimitLabelValue =
+      "You used all ${chatLimitError?.limit} chat messages for this month. Upgrade to Cody Pro to get unlimited chats."
+  val autocompleteLimitLabelValue =
+      "You used all ${autocompleteLimitError?.limit} autocompletions for this month. Upgrade to Cody Pro to get unlimited autocompletions."
+  val chatAndAutocompleteLimitLabelValue =
+      "You used all you autocompletions and chats for this month. Upgrade to Cody Pro to get unlimited interactions."
   if (!isCurrentUserPro) {
-    if (chatLimitError != null && autocompleteLimitError != null) {
-      row {
-        label(
-            "<html>\u26A1 You used all you autocompletions and chats for this month. Upgrade to Cody Pro to get unlimited interactions.<hr><html/>")
-      }
-    } else if (chatLimitError != null) {
-      row {
-        label(
-            "<html>\u26A1 You used all ${chatLimitError.limit} chat messages for this month. Upgrade to Cody Pro to get unlimited chats.<hr><html/>")
-      }
-    } else if (autocompleteLimitError != null) {
-      row {
-        label(
-            "<html>\u26A1 You used all ${autocompleteLimitError.limit} autocompletions for this month. Upgrade to Cody Pro to get unlimited autocompletions.<hr><html/>")
+    row {
+      text(
+          "<html>" +
+              "<table width=\"100%\">" +
+              "<tr>" +
+              "<td width=\"10%\"><span style=\"font-size:20px;\">⚡</span></td>" +
+              "<td width=\"90%\"><p>${
+                if (chatLimitError != null && autocompleteLimitError != null) {
+                  chatAndAutocompleteLimitLabelValue
+                } else {
+                  if (chatLimitError != null) {
+                    chatLimitLabelValue
+                  } else {
+                    autocompleteLimitLabelValue
+                  }
+                }
+                }</p></td>" +
+              "</tr>" +
+              "</table>" +
+              "</html>")
+    }
+    row {
+      if (ThemeUtil.isDarkTheme()) {
+        text("<html><div style=\"height: 1px; background-color: #404245;\"></div></html>")
+      } else {
+        text("<html><div style=\"height: 1px; background-color: #ECEDF1;\"></div></html>")
       }
     }
   }
+
   val tier = if (isCurrentUserPro) "Cody Pro" else "Cody Free"
   row { label("<html>Current tier: <b>$tier</b></html>") }
   row {
