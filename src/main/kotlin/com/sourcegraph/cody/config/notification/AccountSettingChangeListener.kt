f6180590078c13e857a7b290805c33eb0caff7b2
dadlerj <daniel.neal.adler@gmail.com>
2024-01-11 08:03:34 -0800

    Only log CodyInstalled and CodyUninstalled events on installs/uninstalls. Log settings change events on server and access token changes

--- src/main/kotlin/com/sourcegraph/cody/config/notification/AccountSettingChangeListener.kt
+++ src/main/kotlin/com/sourcegraph/cody/config/notification/AccountSettingChangeListener.kt
@@ -18,13 +18,6 @@ class AccountSettingChangeListener(project: Project) : ChangeListener(project) {
     connection.subscribe(
         AccountSettingChangeActionNotifier.TOPIC,
         object : AccountSettingChangeActionNotifier {
-          override fun beforeAction(serverUrlChanged: Boolean) {
-            if (serverUrlChanged) {
-              GraphQlLogger.logUninstallEvent(project)
-              CodyApplicationSettings.instance.isInstallEventLogged = false
-            }
-          }
-
           override fun afterAction(context: AccountSettingChangeContext) {
             val codyApplicationSettings = CodyApplicationSettings.instance
             // Notify JCEF about the config changes
@@ -60,14 +53,9 @@ class AccountSettingChangeListener(project: Project) : ChangeListener(project) {
 
             // Log install events
             if (context.serverUrlChanged) {
-              GraphQlLogger.logInstallEvent(project).thenAccept { e ->
-                codyApplicationSettings.isInstallEventLogged = e
-              }
-            } else if (context.accessTokenChanged &&
-                !codyApplicationSettings.isInstallEventLogged) {
-              GraphQlLogger.logInstallEvent(project).thenAccept { e ->
-                codyApplicationSettings.isInstallEventLogged = e
-              }
+              GraphQlLogger.logCodyEvent(project, "settings.serverURL", "changed)
+            } else if (context.accessTokenChanged) {
+              GraphQlLogger.logCodyEvent(project, "settings.accessToken", "changed)
             }
           }
         })
