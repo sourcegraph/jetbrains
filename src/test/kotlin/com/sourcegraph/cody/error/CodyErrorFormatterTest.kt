bb41869705508cab028428dd9bddcd0b046efcc8
Jakub Blach <jakubblach@gmail.com>
2024-01-12 17:16:40 +0100

    spotless

--- src/test/kotlin/com/sourcegraph/cody/error/CodyErrorFormatterTest.kt
+++ src/test/kotlin/com/sourcegraph/cody/error/CodyErrorFormatterTest.kt
@@ -4,20 +4,23 @@ import junit.framework.TestCase
 
 class CodyErrorFormatterTest : TestCase() {
 
-    fun `test error markdown formatting`() {
-        val error = CodyError(
+  fun `test error markdown formatting`() {
+    val error =
+        CodyError(
             title = "java.lang.NullPointerException",
             pluginVersion = "5.2.18066-nightly",
             ideVersion = "IU-233.11799.241",
             additionalInfo = null,
-            stacktrace = """
+            stacktrace =
+                """
             java.lang.NullPointerException: Exception description
                 at com.example.Some.handle(Some.java:326)
                 ... 54 more
-            """.trimIndent()
-        )
-        val markdown = CodyErrorFormatter.formatToMarkdown(error)
-        val expectedMarkdown = """
+            """
+                    .trimIndent())
+    val markdown = CodyErrorFormatter.formatToMarkdown(error)
+    val expectedMarkdown =
+        """
             Plugin version: ```5.2.18066-nightly```
             IDE version: ```IU-233.11799.241```
             Exception: ```java.lang.NullPointerException```
@@ -27,14 +30,14 @@ class CodyErrorFormatterTest : TestCase() {
                 at com.example.Some.handle(Some.java:326)
                 ... 54 more
             ```
-        """.trimIndent()
-        assertEquals(expectedMarkdown, markdown)
-    }
+        """
+            .trimIndent()
+    assertEquals(expectedMarkdown, markdown)
+  }
 
-    fun `test null report results empty markdown`() {
-        val error = CodyError(null, null, null, null, null)
-        val markdown = CodyErrorFormatter.formatToMarkdown(error)
-        assertEquals("", markdown)
-    }
-
+  fun `test null report results empty markdown`() {
+    val error = CodyError(null, null, null, null, null)
+    val markdown = CodyErrorFormatter.formatToMarkdown(error)
+    assertEquals("", markdown)
+  }
 }
