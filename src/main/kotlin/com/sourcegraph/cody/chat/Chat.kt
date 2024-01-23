042f496e7e5b40bfed7d8434340a71b3bfdf039a
Piotr Kukielka <pkukielka@virtuslab.com>
2024-01-23 17:47:27 +0100

    Reformating

--- src/main/kotlin/com/sourcegraph/cody/chat/Chat.kt
+++ src/main/kotlin/com/sourcegraph/cody/chat/Chat.kt
@@ -13,12 +13,12 @@ import com.sourcegraph.cody.vscode.CancellationToken
 import com.sourcegraph.common.CodyBundle
 import com.sourcegraph.common.CodyBundle.fmt
 import com.sourcegraph.common.UpgradeToCodyProNotification.Companion.isCodyProJetbrains
-import org.eclipse.lsp4j.jsonrpc.ResponseErrorException
-import org.slf4j.LoggerFactory
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.function.Consumer
+import org.eclipse.lsp4j.jsonrpc.ResponseErrorException
+import org.slf4j.LoggerFactory
 
 class Chat {
   val logger = LoggerFactory.getLogger(Chat::class.java)
@@ -43,7 +43,8 @@ class Chat {
           val contextMessages =
               agentChatMessage.contextFiles?.map { contextFile: ContextFile ->
                 ContextMessage(Speaker.ASSISTANT, agentChatMessageText, contextFile)
-              } ?: emptyList()
+              }
+                  ?: emptyList()
           chat.displayUsedContext(contextMessages)
           chat.addMessageToChat(chatMessage)
         } else {
