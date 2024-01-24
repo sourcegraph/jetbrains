9742ea60c8b926a6019de36c4b4a70d8d96a70cb
Jakub Blach <jakubblach@gmail.com>
2024-01-24 16:58:29 +0100

    add todos

--- src/main/kotlin/com/sourcegraph/cody/history/state/ChatState.kt
+++ src/main/kotlin/com/sourcegraph/cody/history/state/ChatState.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.components.BaseState
 
 class ChatState : BaseState() {
 
+  // todo settery non-null
   var panelId: String? by string()
   var replyChatId: String? by string()
   var messages by list<MessageState>()
