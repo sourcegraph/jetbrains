45b4a42b609ba85040b96f94df9edb43b0923b8c
Jakub Blach <jakubblach@gmail.com>
2024-01-26 14:17:13 +0100

    fix

--- src/main/kotlin/com/sourcegraph/cody/history/util/DurationGroupFormatter.kt
+++ src/main/kotlin/com/sourcegraph/cody/history/util/DurationGroupFormatter.kt
@@ -10,12 +10,14 @@ object DurationGroupFormatter {
     val weeks = ChronoUnit.WEEKS.between(since, now).toInt()
     val months = ChronoUnit.MONTHS.between(since, now).toInt()
     val years = ChronoUnit.YEARS.between(since, now).toInt()
+    // order of these conditions looks shuffled, but this covers edge-cases
+    // for example: "last month" has higher priority than "4 weeks ago"
     return when {
       days == 0 -> "Today"
       days == 1 -> "Yesterday"
       days in 2..6 -> "$days days ago"
-      months == 1 -> "Last month"
       weeks == 1 -> "Last week"
+      months == 1 -> "Last month"
       weeks in 2..4 -> "$weeks weeks ago"
       years == 1 -> "Last year"
       months in 2..12 -> "$months months ago"
