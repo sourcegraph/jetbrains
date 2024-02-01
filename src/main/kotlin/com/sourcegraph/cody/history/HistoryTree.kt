39ad2b82c0531179c447481c9f5b28c03be06a4a
spawlak <spawlak@virtuslab.com>
2024-02-01 14:35:25 +0100

    Fixing moving updated chat to different period

--- src/main/kotlin/com/sourcegraph/cody/history/HistoryTree.kt
+++ src/main/kotlin/com/sourcegraph/cody/history/HistoryTree.kt
@@ -72,11 +72,7 @@ class HistoryTree(
         model.reload(root)
       } else {
         val period = root.periods().find { it.periodText == periodText }!!
-        val extended = period.leafs() + LeafNode(chat)
-        val sorted = extended.sortedByDescending { it.chat.getUpdatedTimeAt() }
-        period.removeAllChildren()
-        for (child in sorted) period.add(child)
-        model.reload(period)
+        addChatToPeriodAndSort(period, chat)
       }
     } else {
       val period =
@@ -85,11 +81,34 @@ class HistoryTree(
               .flatMap { it.leafs() }
               .find { it.chat.internalId == chat.internalId }!!
               .parent as PeriodNode
-      val sorted = period.leafs().sortedByDescending { it.chat.getUpdatedTimeAt() }
-      period.removeAllChildren()
-      for (child in sorted) period.add(child)
-      model.reload(period)
+      val periodText = DurationGroupFormatter.format(chat.getUpdatedTimeAt())
+      //      Checking if chat needs to be moved to different period
+      if (period.periodText != periodText) {
+        val filter = period.leafs().filter { it.chat.internalId != chat.internalId }
+        if (filter.isEmpty()) {
+          model.removeNodeFromParent(period)
+        } else {
+          period.removeAllChildren()
+          for (child in filter) period.add(child)
+          model.reload(period)
+        }
+        val per = root.periods().find { it.periodText == periodText }
+        addChatToPeriodAndSort(per!!, chat)
+      } else {
+        val sorted = period.leafs().sortedByDescending { it.chat.getUpdatedTimeAt() }
+        period.removeAllChildren()
+        for (child in sorted) period.add(child)
+        model.reload(period)
+      }
     }
+  }
+
+  private fun addChatToPeriodAndSort(period: PeriodNode, chat: ChatState) {
+    val extended = period.leafs() + LeafNode(chat)
+    val sorted = extended.sortedByDescending { it.chat.getUpdatedTimeAt() }
+    period.removeAllChildren()
+    for (child in sorted) period.add(child)
+    model.reload(period)
   }
 
   private fun selectSelected() {
