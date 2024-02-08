8faa3e8943f2e9bcbb277a531cc7c39509d2f666
Olafur Geirsson <olafurpg@gmail.com>
2024-02-08 16:21:41 +0100

    WIP

---
+++ src/main/kotlin/com/sourcegraph/cody/protocol_generated/CompletionLogID.kt
@@ -0,0 +1,4 @@
+@file:Suppress("FunctionName", "ClassName")
+package com.sourcegraph.cody.protocol_generated
+typealias CompletionLogID = String
+
---
+++ src/main/kotlin/com/sourcegraph/cody/protocol_generated/WorkspaceEditEntryMetadata.kt
@@ -0,0 +1,9 @@
+@file:Suppress("FunctionName", "ClassName")
+package com.sourcegraph.cody.protocol_generated
+data class WorkspaceEditEntryMetadata(
+  var needsConfirmation: Boolean? = null,
+  var label: String? = null,
+  var description: String? = null,
+  var iconPath: Uri? = null,
+)
+
