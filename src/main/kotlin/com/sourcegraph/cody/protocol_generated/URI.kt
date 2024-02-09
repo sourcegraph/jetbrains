4c81ed53607d25fad98f99039f4f43c92a38d300
Olafur Geirsson <olafurpg@gmail.com>
2024-02-09 14:22:13 +0100

    WIP

--- src/main/kotlin/com/sourcegraph/cody/protocol_generated/URI.kt
+++ src/main/kotlin/com/sourcegraph/cody/protocol_generated/URI.kt
@@ -1,12 +1,11 @@
 @file:Suppress("FunctionName", "ClassName")
+
 package com.sourcegraph.cody.protocol_generated
 
 data class Uri(
-  var scheme: String? = null,
-  var authority: String? = null,
-  var path: String? = null,
-  var query: String? = null,
-  var fragment: String? = null,
-  var fsPath: String? = null,
+    var scheme: String? = null,
+    var authority: String? = null,
+    var path: String? = null,
+    var query: String? = null,
+    var fragment: String? = null,
 )
-
