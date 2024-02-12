b5b6674375a564038ceddff24e729a7fde40d6ec
Steve Yegge <stevey@sourcegraph.com>
2024-02-12 01:05:13 -0800

    got e2e roundtrip for commands/document working
    The hard part is done; the rest is mostly UI and plugin work

--- src/main/kotlin/com/sourcegraph/cody/edit/InlineFixupCommandSession.kt
+++ src/main/kotlin/com/sourcegraph/cody/edit/InlineFixupCommandSession.kt
@@ -1,5 +1,6 @@
 package com.sourcegraph.cody.edit
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Document
@@ -13,6 +14,7 @@ import com.sourcegraph.cody.vscode.CancellationToken
  * such as adding a doc string, or fixing up a region according to user instructions.
  */
 abstract class InlineFixupCommandSession(val editor: Editor, val cancellationToken: CancellationToken) {
+  private val logger = Logger.getInstance(InlineFixupCommandSession::class.java)
   protected val controller = InlineFixups.instance
 
   protected var taskId: String? = null
@@ -21,13 +23,13 @@ abstract class InlineFixupCommandSession(val editor: Editor, val cancellationTok
 
   abstract fun getLogger(): Logger
 
-  fun performInlineEdits(params: TextDocumentEditParams) {
+  fun performInlineEdits(edits: List<TextEdit>) {
     if (!controller.isEligibleForInlineEdit(editor)) {
       getLogger().warn("Inline edit not eligible")
       return
     }
     WriteCommandAction.runWriteCommandAction(editor.project ?: return) {
-      for (edit in params.edits) {
+      for (edit in edits) {
         val doc: Document = editor.document
         // TODO: handle options if present (currently just undo bounds)
         when (edit.type) {
@@ -37,23 +39,33 @@ abstract class InlineFixupCommandSession(val editor: Editor, val cancellationTok
           else -> getLogger().warn("Unknown edit type: ${edit.type}")
         }
       }
+      ApplicationManager.getApplication().invokeLater {
+        // TODO: Fix up selection.
+        editor.caretModel.primaryCaret.removeSelection()
+      }
     }
-    // TODO: Fix up selection.
-    editor.caretModel.primaryCaret.removeSelection()
+  }
+
+  private fun getOffsets(doc: Document, edit: TextEdit): Pair<Int, Int>? {
+    if (edit.range == null) {
+      logger.warn("Invalid edit range: ${edit.range} for edit: ${edit.type}")
+      return null
+    }
+    return edit.range.toOffsets(doc)
   }
 
   private fun performReplace(doc: Document, edit: TextEdit) {
-    val (start, end) = edit.range?.toOffsets(doc) ?: return
+    val (start, end) = getOffsets(doc, edit) ?: return
     doc.replaceString(start, end, edit.value ?: return)
   }
 
   private fun performInsert(doc: Document, edit: TextEdit) {
-    val start = edit.range?.start?.toOffset(doc) ?: return
+    val (start, _) = getOffsets(doc, edit) ?: return
     doc.insertString(start, edit.value ?: return)
   }
 
   private fun performDelete(doc: Document, edit: TextEdit) {
-    val (start, end) = edit.range?.toOffsets(doc) ?: return
+    val (start, end) = getOffsets(doc, edit) ?: return
     doc.deleteString(start, end)
   }
 
