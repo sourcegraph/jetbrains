6728b8edf3bc692c984cd4cde594dca8f9d251da
Steve Yegge <stevey@sourcegraph.com>
2024-02-13 22:55:14 -0800

    removed garbage Cody put into the file
    Professional hazard when you're implementing inline edits.

--- src/main/java/com/sourcegraph/cody/chat/CodeEditorFactory.java
+++ src/main/java/com/sourcegraph/cody/chat/CodeEditorFactory.java
@@ -169,10 +169,7 @@ public class CodeEditorFactory {
   private ActionListener copyCodeListener(EditorEx editor, JButton copyButton) {
     return e -> {
       String text = editor.getDocument().getText();
-      StringSelection/**
- * There is no code selected, so I cannot generate documentation for it.
- */
- stringSelection = new StringSelection(text);
+      StringSelection stringSelection = new StringSelection(text);
       Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
       clipboard.setContents(stringSelection, null);
       copyButton.setText("Copied");
