52cee47636d73f5c81a031b960bf9294f14fd9a5
Olafur Geirsson <olafurpg@gmail.com>
2024-02-14 16:00:07 +0100

    WIP

--- src/main/kotlin/com/sourcegraph/cody/agent/protocol_extensions/TextDocumentBuilder.kt
+++ src/main/kotlin/com/sourcegraph/cody/agent/protocol_extensions/TextDocumentBuilder.kt
@@ -1,19 +1,27 @@
 package com.sourcegraph.cody.agent.protocol_extensions
 
+import com.sourcegraph.cody.protocol_generated.ChatError
+import com.sourcegraph.cody.protocol_generated.ChatMessage
 import com.sourcegraph.cody.protocol_generated.ProtocolTextDocument
 import com.sourcegraph.cody.protocol_generated.Range
 import java.nio.file.Paths
 
-object TextDocumentBuilder {
-  @JvmStatic
-  @JvmOverloads
-  fun fromPath(
-      path: String,
-      content: String? = null,
-      selection: Range? = null
-  ): ProtocolTextDocument {
-    val uri = Paths.get(path).toUri().toString()
-    val rfc3986Uri = Rfc3986UriEncoder.encode(uri)
-    return ProtocolTextDocument(uri = rfc3986Uri, content = content, selection = selection)
-  }
+fun ProtocolTextDocument.fromPath(
+    path: String,
+    content: String? = null,
+    selection: Range? = null
+): ProtocolTextDocument {
+  val uri = Paths.get(path).toUri().toString()
+  val rfc3986Uri = Rfc3986UriEncoder.encode(uri)
+  return ProtocolTextDocument(uri = rfc3986Uri, content = content, selection = selection)
 }
+
+fun ChatError.toRateLimitError(): RateLimitError? {
+  val upgradeIsAvailable = this.upgradeIsAvailable ?: return null
+  return RateLimitError(
+      upgradeIsAvailable = upgradeIsAvailable,
+      limit = this.limit,
+  )
+}
+
+fun ChatMessage.actualMessage(): String = this.displayText ?: this.text ?: ""
