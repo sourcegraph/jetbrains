19b7f91b6ecd33d574e133d19100f48f3910eae0
Steve Yegge <stevey@sourcegraph.com>
2024-02-18 19:26:06 -0800

    added support for Undo
    Not fully wired up until the selection problem is fixed

---
+++ src/main/kotlin/com/sourcegraph/cody/edit/FixupUndoableAction.kt
@@ -0,0 +1,98 @@
+package com.sourcegraph.cody.edit
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.command.undo.DocumentReference
+import com.intellij.openapi.command.undo.DocumentReferenceManager
+import com.intellij.openapi.command.undo.UndoableAction
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.Editor
+import com.sourcegraph.cody.agent.protocol.TextEdit
+
+abstract class FixupUndoableAction(val editor: Editor, val edit: TextEdit) : UndoableAction {
+  private val logger = Logger.getInstance(FixupUndoableAction::class.java)
+
+  val document: Document = editor.document
+
+  override fun getAffectedDocuments(): Array<out DocumentReference> {
+    val documentReference = DocumentReferenceManager.getInstance().create(document)
+    return arrayOf(documentReference)
+  }
+
+  override fun isGlobal(): Boolean {
+    return true
+  }
+
+  class InsertUndoableAction(editor: Editor, edit: TextEdit) : FixupUndoableAction(editor, edit) {
+
+    override fun undo() {
+      val offsets = (edit.range ?: return).toOffsets(document)
+      ApplicationManager.getApplication().runWriteAction {
+        document.deleteString(offsets.first, offsets.second)
+      }
+    }
+
+    override fun redo() {
+      val offset = edit.position?.toOffset(document) ?: return
+      ApplicationManager.getApplication().runWriteAction {
+        document.insertString(offset, edit.value ?: "")
+      }
+    }
+  }
+
+  class ReplaceUndoableAction(editor: Editor, edit: TextEdit) : FixupUndoableAction(editor, edit) {
+
+    override fun undo() {
+      TODO("fix")
+    }
+
+    override fun redo() {
+      TODO("fix")
+    }
+  }
+
+  class DeleteUndoableAction(editor: Editor, edit: TextEdit) : FixupUndoableAction(editor, edit) {
+    var oldText = ""
+
+    override fun undo() {
+      val offsets = (edit.range ?: return).toOffsets(document)
+      val deleted = document.text.substring(offsets.first, offsets.second)
+      ApplicationManager.getApplication().runWriteAction {
+        document.replaceString(offsets.first, offsets.second, oldText)
+      }
+      oldText = deleted
+    }
+
+    override fun redo() {
+      val offsets = (edit.range ?: return).toOffsets(document)
+      ApplicationManager.getApplication().runWriteAction {
+        document.deleteString(offsets.first, offsets.second)
+      }
+      oldText = ""
+    }
+  }
+
+  class NoOpUndoableAction(editor: Editor, edit: TextEdit) : FixupUndoableAction(editor, edit) {
+    private val logger = Logger.getInstance(NoOpUndoableAction::class.java)
+
+    override fun undo() {
+      logger.warn("Received invalid undoable action for $edit")
+    }
+
+    override fun redo() {
+      logger.warn("Received invalid undoable action for $edit")
+    }
+  }
+
+  companion object {
+
+    fun from(editor: Editor, edit: TextEdit): FixupUndoableAction {
+      return when (edit.type) {
+        "insert" -> InsertUndoableAction(editor, edit)
+        "replace" -> ReplaceUndoableAction(editor, edit)
+        "delete" -> DeleteUndoableAction(editor, edit)
+        else -> NoOpUndoableAction(editor, edit)
+      }
+    }
+  }
+}
