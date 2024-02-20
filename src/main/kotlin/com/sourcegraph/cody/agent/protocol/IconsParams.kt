efabe1378920ca1460377383cbf813928ab6d2fd
Steve Yegge <stevey@sourcegraph.com>
2024-02-20 11:05:53 -0800

    Updated protocol bindings for PR 3219
    ProtocolCommand title is now an object

---
+++ src/main/kotlin/com/sourcegraph/cody/agent/protocol/IconsParams.kt
@@ -0,0 +1,7 @@
+@file:Suppress("FunctionName", "ClassName", "unused", "EnumEntryName", "UnusedImport")
+package com.sourcegraph.cody.agent
+
+data class IconsParams(
+  val value: String? = null,
+  val position: Int? = null,
+)
