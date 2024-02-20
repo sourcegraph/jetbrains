2b69a69b48b916ee7cb1e43cedf2d8cfefead336
Steve Yegge <stevey@sourcegraph.com>
2024-02-19 17:54:28 -0800

    Got Accept, Undo and Cancel working with telemetry

--- src/main/kotlin/com/sourcegraph/cody/edit/widget/LensWidget.kt
+++ src/main/kotlin/com/sourcegraph/cody/edit/widget/LensWidget.kt
@@ -1,6 +1,7 @@
 package com.sourcegraph.cody.edit.widget
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.util.Disposer
 import java.awt.FontMetrics
@@ -10,27 +11,18 @@ abstract class LensWidget(val parentGroup: LensWidgetGroup) : Disposable {
   protected val logger = Logger.getInstance(LensWidget::class.java)
   protected var mouseInBounds = false
 
-  init {
-    Disposer.register(parentGroup, this)
-  }
-
   abstract fun calcWidthInPixels(fontMetrics: FontMetrics): Int
 
   abstract fun calcHeightInPixels(fontMetrics: FontMetrics): Int
 
   abstract fun paint(g: Graphics2D, x: Float, y: Float)
 
-  /**
-   * Optional method for updating the widget state, useful for animations.
-   */
+  /** Optional method for updating the widget state, useful for animations. */
   open fun update() {
     // Default implementation does nothing
   }
 
-  /**
-   * Called only when widget is clicked.
-   * Coordinates are relative to the widget.
-   */
+  /** Called only when widget is clicked. Coordinates are relative to the widget. */
   open fun onClick(x: Int, y: Int): Boolean {
     return false
   }
@@ -38,6 +30,7 @@ abstract class LensWidget(val parentGroup: LensWidgetGroup) : Disposable {
   open fun onMouseEnter() {
     mouseInBounds = true
   }
+
   open fun onMouseExit() {
     mouseInBounds = false
   }
