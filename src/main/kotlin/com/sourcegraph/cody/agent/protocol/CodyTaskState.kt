188d861a54f204bd32a2ede93f3003d38a3ba8b0
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-03-14 00:19:16 +0900

    Demo scree

--- src/main/kotlin/com/sourcegraph/cody/agent/protocol/CodyTaskState.kt
+++ src/main/kotlin/com/sourcegraph/cody/agent/protocol/CodyTaskState.kt
@@ -1,21 +1,22 @@
 package com.sourcegraph.cody.agent.protocol
 
-enum class CodyTaskState(val value: String) {
-  idle("idle"),
-  working("working"),
-  inserting("inserting"),
-  applying("applying"),
-  formatting("formatting"),
-  applied("applied"),
-  finished("finished"),
-  error("error"),
-  pending("pending")
+enum class CodyTaskState(val id: Int) {
+  Idle(0),
+  Working(1),
+  Inserting(2),
+  Applying(3),
+  Formatting(4),
+  Applied(5),
+  Finished(6),
+  Error(7),
+  Pending(8)
 }
+
 
 val CodyTaskState.isTerminal
   get() =
       when (this) {
-        CodyTaskState.finished,
-        CodyTaskState.error -> true
+        CodyTaskState.Finished,
+        CodyTaskState.Error -> true
         else -> false
       }
