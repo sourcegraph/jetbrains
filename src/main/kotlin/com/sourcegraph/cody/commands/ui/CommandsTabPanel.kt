6da2f4b513b77df7a27d1a6b0ca694c608a6e3c8
Piotr Kukielka <pkukielka@virtuslab.com>
2024-04-10 15:22:56 +0200

    Add edit and document commands in the Commands tab

--- src/main/kotlin/com/sourcegraph/cody/commands/ui/CommandsTabPanel.kt
+++ src/main/kotlin/com/sourcegraph/cody/commands/ui/CommandsTabPanel.kt
@@ -10,8 +10,9 @@ import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
 import com.intellij.ui.components.JBPanelWithEmptyText
-import com.sourcegraph.cody.autocomplete.CodyEditorFactoryListener
 import com.sourcegraph.cody.commands.CommandId
+import com.sourcegraph.cody.config.CodyApplicationSettings
+import com.sourcegraph.config.ConfigUtil
 import java.awt.Component
 import java.awt.Dimension
 import java.awt.GridLayout
@@ -27,46 +28,39 @@ class CommandsTabPanel(
   init {
     layout = BoxLayout(this, BoxLayout.Y_AXIS)
     CommandId.values().forEach { command -> addCommandButton(command) }
-    addInlineEditActionButton("cody.editCodeAction")
-    addInlineEditActionButton("cody.documentCodyAction")
-  }
 
-  private fun executeCommandWithContext(commandId: CommandId) {
-    FileEditorManager.getInstance(project).selectedTextEditor?.let {
-      CodyEditorFactoryListener.Util.informAgentAboutEditorChange(it, hasFileChanged = false) {
-        executeCommand(commandId)
-      }
+    if (ConfigUtil.isFeatureFlagEnabled("cody.feature.inline-edits") ||
+        CodyApplicationSettings.instance.isInlineEditionEnabled) {
+      addInlineEditActionButton("cody.editCodeAction")
+      addInlineEditActionButton("cody.documentCodeAction")
     }
   }
 
   private fun addInlineEditActionButton(actionId: String) {
     val action = ActionManagerEx.getInstanceEx().getAction(actionId)
-    val editor = FileEditorManager.getInstance(project).selectedTextEditor ?: return
-    val dataContext = (editor as? EditorEx)?.dataContext ?: return
-
     addButton(action.templatePresentation.text, action.templatePresentation.mnemonic) {
+      val editor = FileEditorManager.getInstance(project).selectedTextEditor ?: return@addButton
+      val dataContext = (editor as? EditorEx)?.dataContext ?: return@addButton
       val managerEx = ActionManagerEx.getInstanceEx()
       val event =
           AnActionEvent(null, dataContext, ActionPlaces.UNKNOWN, Presentation(), managerEx, 0)
-      if (!ActionUtil.lastUpdateAndCheckDumb(action, event, false)) {
-        return@addButton
-      }
       ActionUtil.performActionDumbAwareWithCallbacks(action, event)
     }
   }
 
   private fun addCommandButton(commandId: CommandId) {
-    addButton(commandId.displayName, commandId.mnemonic) { executeCommandWithContext(commandId) }
+    addButton(commandId.displayName, commandId.mnemonic) { executeCommand(commandId) }
   }
 
   private fun addButton(displayName: String, mnemonic: Int, action: () -> Unit) {
     val button = JButton(displayName)
-    button.setMnemonic(mnemonic)
+    button.mnemonic = mnemonic
+    button.displayedMnemonicIndex = displayName.indexOfFirst { it.code == mnemonic }
     button.alignmentX = Component.CENTER_ALIGNMENT
     button.maximumSize = Dimension(Int.MAX_VALUE, button.getPreferredSize().height)
     val buttonUI = DarculaButtonUI.createUI(button) as ButtonUI
     button.setUI(buttonUI)
-    button.addActionListener { event -> action() }
+    button.addActionListener { action() }
     add(button)
   }
 }
