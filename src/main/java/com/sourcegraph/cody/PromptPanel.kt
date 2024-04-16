sourcegraph/cody

commit 88a21694097716fbe53b84e76b3124cb3454af76

Refactor chat to allow multiple parallel sessions run in parallel (#365)
## Test plan

Manual execution of all steps from the QA guide.

## Changes

This PR is needed to enable real multi-chats. 
Our app written with only one chat window/session in mind and there were
a lot of UI assumptions and global state which would block multi-chat
implementation.

Notable changes in this PR:
* Introduced `ChatSession` class which manages whole lifecycle of a
single chat session
* Fixed protocol bug in chat restore which was causing it to lose chat
history
* Eliminated shared state from `CodyToolWindowContent`
* Decreased size of `CodyToolWindowContent` monolith by few times
* Fixed handling of no panel errors by properly restarting sessions on
agent (re)start
* Improved class organisation by moving UI-only classes into separate
package and extracting UI logic into few new classes
* Simplified control flow by having just simple
send/receive/cancellation token (see: `ChatSession` interface) and no
global chat state/flow control
* With all the above still removed over 100 lines of code ;)

---------

Co-authored-by: Mikołaj Kondratek <mik.kondratek@gmail.com>

src/main/java/com/sourcegraph/cody/PromptPanel.kt
@@ -4,92 +4,93 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CustomShortcutSet
 import com.intellij.openapi.actionSystem.KeyboardShortcut
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.ui.DocumentAdapter
+import com.intellij.util.concurrency.annotations.RequiresEdt
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
+import com.sourcegraph.cody.chat.ChatSession
 import com.sourcegraph.cody.chat.CodyChatMessageHistory
+import com.sourcegraph.cody.chat.ui.SendButton
 import com.sourcegraph.cody.ui.AutoGrowingTextArea
+import com.sourcegraph.cody.vscode.CancellationToken
 import java.awt.Dimension
 import java.awt.event.ComponentAdapter
 import java.awt.event.ComponentEvent
 import java.awt.event.KeyAdapter
 import java.awt.event.KeyEvent
-import javax.swing.JButton
 import javax.swing.JLayeredPane
 import javax.swing.KeyStroke
 import javax.swing.border.EmptyBorder
+import javax.swing.event.AncestorEvent
+import javax.swing.event.AncestorListener
 import javax.swing.event.DocumentEvent
-import javax.swing.text.DefaultEditorKit
 
-class PromptPanel(
-    chatMessageHistory: CodyChatMessageHistory,
-    onSendMessageAction: () -> Unit,
-    val sendButton: JButton,
-    isGenerating: () -> Boolean,
-) : JLayeredPane() {
+class PromptPanel(private val chatSession: ChatSession) : JLayeredPane() {
 
-  private var isInHistoryMode = true
   private val autoGrowingTextArea = AutoGrowingTextArea(5, 9, this)
   private val scrollPane = autoGrowingTextArea.scrollPane
-  private val margin = 14
-  val textArea = autoGrowingTextArea.textArea
+  private val textArea = autoGrowingTextArea.textArea
+  private val promptMessageHistory = CodyChatMessageHistory(CHAT_MESSAGE_HISTORY_CAPACITY)
+  private val sendButton = SendButton()
 
   init {
     textArea.emptyText.text = "Ask a question about this code..."
-    textArea.border = EmptyBorder(JBUI.insets(4, 4, 4, 24))
+
+    sendButton.addActionListener { _ -> chatSession.sendMessage(getTextAndReset()) }
 
     val upperMessageAction: AnAction =
         object : DumbAwareAction() {
           override fun actionPerformed(e: AnActionEvent) {
-            if (isInHistoryMode) {
-              chatMessageHistory.popUpperMessage(textArea)
-            } else {
-              val defaultAction = textArea.actionMap[DefaultEditorKit.upAction]
-              defaultAction.actionPerformed(null)
-            }
+            promptMessageHistory.popUpperMessage(textArea)
           }
         }
     val lowerMessageAction: AnAction =
         object : DumbAwareAction() {
           override fun actionPerformed(e: AnActionEvent) {
-            if (isInHistoryMode) {
-              chatMessageHistory.popLowerMessage(textArea)
-            } else {
-              val defaultAction = textArea.actionMap[DefaultEditorKit.downAction]
-              defaultAction.actionPerformed(null)
-            }
+            promptMessageHistory.popLowerMessage(textArea)
           }
         }
     val sendMessageAction: AnAction =
         object : DumbAwareAction() {
           override fun actionPerformed(e: AnActionEvent) {
             if (sendButton.isEnabled) {
-              onSendMessageAction()
-              isInHistoryMode = true
+              chatSession.sendMessage(getTextAndReset())
             }
           }
         }
+
+    addAncestorListener(
+        object : AncestorListener {
+          override fun ancestorAdded(event: AncestorEvent?) {
+            textArea.requestFocusInWindow()
+            textArea.caretPosition = textArea.document.length
+          }
+
+          override fun ancestorRemoved(event: AncestorEvent?) {}
+
+          override fun ancestorMoved(event: AncestorEvent?) {}
+        })
+
     sendMessageAction.registerCustomShortcutSet(DEFAULT_SUBMIT_ACTION_SHORTCUT, textArea)
     upperMessageAction.registerCustomShortcutSet(POP_UPPER_MESSAGE_ACTION_SHORTCUT, textArea)
     lowerMessageAction.registerCustomShortcutSet(POP_LOWER_MESSAGE_ACTION_SHORTCUT, textArea)
+
     textArea.addKeyListener(
         object : KeyAdapter() {
           override fun keyReleased(e: KeyEvent) {
             val keyCode = e.keyCode
-            if (keyCode != KeyEvent.VK_UP && keyCode != KeyEvent.VK_DOWN) {
-              isInHistoryMode = textArea.getText().isEmpty()
-            }
+            if (keyCode != KeyEvent.VK_UP && keyCode != KeyEvent.VK_DOWN) {}
           }
         })
     textArea.document.addDocumentListener(
         object : DocumentAdapter() {
           override fun textChanged(e: DocumentEvent) {
-            val empty = textArea.getText().isEmpty()
-            sendButton.isEnabled = !empty && !isGenerating()
+            refreshSendButton()
           }
         })
-    scrollPane.border = EmptyBorder(JBUI.insets(0, margin, margin, margin))
+    scrollPane.border = EmptyBorder(JBUI.emptyInsets())
     scrollPane.background = UIUtil.getPanelBackground()
 
     // Set initial bounds for the scrollPane (100x100) to ensure proper initialization;
@@ -100,7 +101,7 @@ class PromptPanel(
 
     add(sendButton, PALETTE_LAYER, 0)
 
-    scrollPane.setBounds(0, 0, width, scrollPane.preferredSize.height + margin)
+    scrollPane.setBounds(0, 0, width, scrollPane.preferredSize.height)
 
     preferredSize = Dimension(scrollPane.width, scrollPane.height)
 
@@ -110,27 +111,42 @@ class PromptPanel(
             revalidate()
             val jButtonPreferredSize = sendButton.preferredSize
             sendButton.setBounds(
-                scrollPane.width - jButtonPreferredSize.width - margin,
-                scrollPane.height - jButtonPreferredSize.height - margin,
+                scrollPane.width - jButtonPreferredSize.width,
+                scrollPane.height - jButtonPreferredSize.height,
                 jButtonPreferredSize.width,
                 jButtonPreferredSize.height)
           }
         })
   }
 
+  @RequiresEdt
+  fun refreshSendButton() {
+    sendButton.isEnabled =
+        textArea.getText().isNotEmpty() && chatSession.getCancellationToken().isDone
+  }
+
+  fun registerCancellationToken(cancellationToken: CancellationToken) {
+    cancellationToken.onFinished {
+      ApplicationManager.getApplication().invokeLater { refreshSendButton() }
+    }
+  }
+
   override fun revalidate() {
     super.revalidate()
 
-    scrollPane.setBounds(0, 0, width, scrollPane.preferredSize.height + margin)
+    scrollPane.setBounds(0, 0, width, scrollPane.preferredSize.height)
     preferredSize = Dimension(scrollPane.width, scrollPane.height)
   }
 
-  fun reset() {
+  private fun getTextAndReset(): String {
+    val text = textArea.text
+    promptMessageHistory.messageSent(text)
     textArea.text = ""
-    isInHistoryMode = true
+    return text
   }
 
   companion object {
+    private const val CHAT_MESSAGE_HISTORY_CAPACITY = 100
     private val JUST_ENTER = KeyboardShortcut(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), null)
 
     val UP = KeyboardShortcut(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), null)

src/main/java/com/sourcegraph/cody/CodyToolWindowFactory.java
@@ -29,15 +29,8 @@ public class CodyToolWindowFactory implements ToolWindowFactory, DumbAware {
               // ContentFactory.SERVICE.getInstance() has been deprecated in recent versions
               ApplicationManager.getApplication()
                   .getService(ContentFactory.class)
-                  .createContent(toolWindowContent.getContentPanel(), "", false);
-          content.setPreferredFocusableComponent(
-              toolWindowContent.getPreferredFocusableComponent());
-          toolWindowContent.addToTabbedPaneChangeListener(
-              () -> {
-                content.setPreferredFocusableComponent(
-                    toolWindowContent.getPreferredFocusableComponent());
-                return null;
-              });
+                  .createContent(toolWindowContent.getAllContentPanel(), "", false);
+          content.setPreferredFocusableComponent(null);
           toolWindow.getContentManager().addContent(content);
           DefaultActionGroup customCodySettings = new DefaultActionGroup();
           customCodySettings.add(new OpenPluginSettingsAction("Cody Settings..."));

src/main/java/com/sourcegraph/cody/LowerPanel.kt
@@ -1,20 +1,0 @@
-package com.sourcegraph.cody
-
-import com.intellij.openapi.ui.VerticalFlowLayout
-import com.sourcegraph.cody.context.ui.EnhancedContextPanel
-import java.awt.BorderLayout
-import javax.swing.JPanel
-
-class LowerPanel(
-    stopGeneratingButtonPanel: JPanel,
-    promptPanel: PromptPanel,
-    contextView: EnhancedContextPanel
-) : JPanel(BorderLayout()) {
-  init {
-    border = contextView.border
-    layout = VerticalFlowLayout()
-    add(stopGeneratingButtonPanel)
-    add(promptPanel)
-    add(contextView)
-  }
-}

src/main/java/com/sourcegraph/cody/SubscriptionTabUtils.kt
@@ -1,5 +1,6 @@
 package com.sourcegraph.cody
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
 import com.sourcegraph.cody.agent.CodyAgentServer
@@ -19,33 +20,41 @@ fun fetchSubscriptionPanelData(
     server: CodyAgentServer
 ): CompletableFuture<SubscriptionTabPanelData?> {
   val activeAccountType = CodyAuthenticationManager.instance.getActiveAccount(project)
+  val result = CompletableFuture<SubscriptionTabPanelData?>()
+
   if (activeAccountType != null) {
     ensureUserIdMatchInAgent(activeAccountType.id, server)
-    return if (activeAccountType.isDotcomAccount()) {
-      val codyProFeatureFlag =
-          server.evaluateFeatureFlag(GetFeatureFlag.CodyProJetBrains).get() == true
-      if (codyProFeatureFlag) {
-        val isCurrentUserPro = getIsCurrentUserPro(server) ?: false
-        CompletableFuture.completedFuture(
-            SubscriptionTabPanelData(
-                activeAccountType.isDotcomAccount(),
-                codyProFeatureFlag = true,
-                isCurrentUserPro = isCurrentUserPro))
-      } else {
-        CompletableFuture.completedFuture(
-            SubscriptionTabPanelData(
-                activeAccountType.isDotcomAccount(),
-                codyProFeatureFlag = false,
-                isCurrentUserPro = null))
+
+    if (activeAccountType.isDotcomAccount()) {
+      ApplicationManager.getApplication().executeOnPooledThread {
+        val codyProFeatureFlag =
+            server.evaluateFeatureFlag(GetFeatureFlag.CodyProJetBrains).get() == true
+        if (codyProFeatureFlag) {
+          val isCurrentUserPro = getIsCurrentUserPro(server) ?: false
+          result.complete(
+              SubscriptionTabPanelData(
+                  activeAccountType.isDotcomAccount(),
+                  codyProFeatureFlag = true,
+                  isCurrentUserPro = isCurrentUserPro))
+        } else {
+          result.complete(
+              SubscriptionTabPanelData(
+                  activeAccountType.isDotcomAccount(),
+                  codyProFeatureFlag = false,
+                  isCurrentUserPro = null))
+        }
       }
     } else {
-      CompletableFuture.completedFuture(
+      result.complete(
           SubscriptionTabPanelData(
               activeAccountType.isDotcomAccount(),
               codyProFeatureFlag = false,
               isCurrentUserPro = false))
     }
+
+    return result
   }
+
   return CompletableFuture.completedFuture(null)
 }
 

src/main/java/com/sourcegraph/cody/agent/CodyAgentClient.java
@@ -3,15 +3,12 @@ package com.sourcegraph.cody.agent;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
-import com.sourcegraph.cody.agent.protocol.ChatMessage;
 import com.sourcegraph.cody.agent.protocol.DebugMessage;
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 import org.eclipse.lsp4j.jsonrpc.services.JsonNotification;
 import org.eclipse.lsp4j.jsonrpc.services.JsonRequest;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /** Implementation of the client part of the Cody agent protocol. */
@@ -20,8 +17,7 @@ public class CodyAgentClient {
 
   private static final Logger logger = Logger.getInstance(CodyAgentClient.class);
   // Callback that is invoked when the agent sends a "chat/updateMessageInProgress" notification.
-  @Nullable public Consumer<ChatMessage> onChatUpdateMessageInProgress;
-  @NotNull public Runnable onFinishedProcessing = () -> {};
+  @Nullable public Consumer<WebviewPostMessageParams> onNewMessage;
   @Nullable public Editor editor;
 
   /**
@@ -47,17 +43,6 @@ public class CodyAgentClient {
   // Notifications
   // =============
 
-  @JsonNotification("chat/updateMessageInProgress")
-  public void chatUpdateMessageInProgress(ChatMessage params) {
-    if (onChatUpdateMessageInProgress != null && params != null) {
-      ApplicationManager.getApplication()
-          .invokeLater(() -> onChatUpdateMessageInProgress.accept(params));
-    }
-    if (params == null) {
-      onFinishedProcessing.run();
-    }
-  }
-
   @JsonNotification("debug/message")
   public void debugMessage(DebugMessage msg) {
     logger.warn(String.format("%s: %s", msg.getChannel(), msg.getMessage()));
@@ -72,25 +57,14 @@ public class CodyAgentClient {
 
   @JsonNotification("webview/postMessage")
   public void webviewPostMessage(WebviewPostMessageParams params) {
-    if (onChatUpdateMessageInProgress != null
-        && params.getMessage().getType().equals(ExtensionMessage.Type.TRANSCRIPT)) {
-      if (Boolean.FALSE.equals(params.getMessage().isMessageInProgress())) {
-        onFinishedProcessing.run();
-      } else if (params.getMessage().getMessages() != null
-          && !params.getMessage().getMessages().isEmpty()) {
-        ApplicationManager.getApplication()
-            .invokeLater(
-                () ->
-                    onChatUpdateMessageInProgress.accept(
-                        Objects.requireNonNull(params.getMessage().getMessages())
-                            .get(params.getMessage().getMessages().size() - 1)));
+    ExtensionMessage extensionMessage = params.getMessage();
 
-      } else {
-        logger.warn("webview/postMessage: no messages in transcript");
-      }
+    if (onNewMessage != null
+        && extensionMessage.getType().equals(ExtensionMessage.Type.TRANSCRIPT)) {
+      ApplicationManager.getApplication().invokeLater(() -> onNewMessage.accept(params));
     } else {
-      logger.warn("onChatUpdateMessageInProgress is null or message type is not transcript");
-      logger.warn(String.format("webview/postMessage %s: %s", params.getId(), params.getMessage()));
+      logger.debug("onNewMessage is null or message type is not transcript");
+      logger.debug(String.format("webview/postMessage %s: %s", params.getId(), extensionMessage));
     }
   }
 }

src/main/java/com/sourcegraph/cody/agent/WebviewPostMessageParams.kt
@@ -10,8 +10,8 @@ import com.sourcegraph.cody.agent.protocol.ContextFile
  */
 data class WebviewMessage(
     val command: String,
-    val text: String,
-    val submitType: String, // One of: "user", "suggestion", "example"
+    val text: String? = null,
+    val submitType: String? = null, // One of: "user", "suggestion", "example"
     val addEnhancedContext: Boolean? = null,
     val contextFiles: List<ContextFile>? = null,
     val error: ChatError? = null,

src/main/java/com/sourcegraph/cody/chat/CodeEditorFactory.java
@@ -17,6 +17,8 @@ import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.IdeFocusManager;
 import com.intellij.util.ui.JBInsets;
+import com.sourcegraph.cody.chat.ui.CodeEditorButtons;
+import com.sourcegraph.cody.chat.ui.CodeEditorPart;
 import com.sourcegraph.cody.ui.TransparentButton;
 import java.awt.Dimension;
 import java.awt.Insets;

src/main/java/com/sourcegraph/cody/chat/MessageContentCreatorFromMarkdownNodes.java
@@ -1,6 +1,7 @@
 package com.sourcegraph.cody.chat;
 
 import com.intellij.util.concurrency.annotations.RequiresEdt;
+import com.sourcegraph.cody.chat.ui.SingleMessagePanel;
 import org.commonmark.node.*;
 import org.commonmark.node.Image;
 import org.commonmark.renderer.html.HtmlRenderer;
@@ -14,11 +15,11 @@ import org.jetbrains.annotations.Nullable;
  */
 public class MessageContentCreatorFromMarkdownNodes extends AbstractVisitor {
   private final HtmlRenderer htmlRenderer;
-  private final MessagePanel messagePanel;
+  private final SingleMessagePanel messagePanel;
   private StringBuilder htmlContent = new StringBuilder();
 
   public MessageContentCreatorFromMarkdownNodes(
-      @NotNull MessagePanel messagePanel, @NotNull HtmlRenderer htmlRenderer) {
+      @NotNull SingleMessagePanel messagePanel, @NotNull HtmlRenderer htmlRenderer) {
     this.messagePanel = messagePanel;
     this.htmlRenderer = htmlRenderer;
   }

sourcegraph/jetbrains

commit 78a8118e510bbe47ea56d82860481acce7cd1f15

Fix bug where up/down arrow keys always navigate chat history
Fixes #1134

Previously, pressing up/down arrow keys in the chat input always
replaced the entire contents of the chat input with the previous/next
chat message from history. This was a regression that was introduced
in the commit https://sourcegraph.com/github.com/sourcegraph/jetbrains/-/commit/88a21694097716fbe53b84e76b3124cb3454af76#diff-91c99de25d8f01dab68bee8fa28fb6f1L80
where the variable `isInHistoryMode` got removed. This PR adds back the
original logic for `isInHistoryMode` so the chat input works like
before.

Demo https://www.loom.com/share/4806fcaeaa9e44519d12e8b6d16a29a0

src/main/java/com/sourcegraph/cody/PromptPanel.kt
@@ -28,6 +28,7 @@ import javax.swing.KeyStroke
 import javax.swing.border.EmptyBorder
 import javax.swing.event.AncestorEvent
 import javax.swing.event.AncestorListener
+import javax.swing.text.DefaultEditorKit
 
 class PromptPanel(project: Project, private val chatSession: ChatSession) : JLayeredPane() {
 
@@ -39,6 +40,11 @@ class PromptPanel(project: Project, private val chatSession: ChatSession) : JLay
   private var contextFilesListViewModel = DefaultListModel<DisplayedContextFile>()
   private val contextFilesListView = JBList(contextFilesListViewModel)
   private val contextFilesContainer = JBScrollPane(contextFilesListView)
+  // When "history mode" is enabled, pressing Up/Down arrow keys replaces the chat input with the
+  // previous/next chat message (via CodyChatMessageHistory). History mode can only activated when
+  // the chat input is empty, and it's deactivated on the first key action that is not an Up/Down
+  // arrow key.
+  private var isInHistoryMode = true
 
   /** Externally updated state */
   private val selectedContextItems: ArrayList<ContextItem> = ArrayList()
@@ -91,6 +97,15 @@ class PromptPanel(project: Project, private val chatSession: ChatSession) : JLay
       refreshSendButton()
       didUserInputChange()
     }
+    textArea.addKeyListener(
+        object : KeyAdapter() {
+          override fun keyReleased(e: KeyEvent) {
+            if (e.keyCode != KeyEvent.VK_UP && e.keyCode != KeyEvent.VK_DOWN) {
+              isInHistoryMode = textArea.getText().isEmpty()
+            }
+          }
+        })
+
     contextFilesListView.addMouseListener(
         object : MouseAdapter() {
           override fun mouseClicked(e: MouseEvent) {
@@ -125,8 +140,20 @@ class PromptPanel(project: Project, private val chatSession: ChatSession) : JLay
     }
     when (shortcut) {
       ENTER -> if (sendButton.isEnabled) didSubmitChatMessage()
-      UP -> promptMessageHistory.popUpperMessage(textArea)
-      DOWN -> promptMessageHistory.popLowerMessage(textArea)
+      UP ->
+          if (isInHistoryMode) {
+            promptMessageHistory.popUpperMessage(textArea)
+          } else {
+            val defaultAction = textArea.actionMap[DefaultEditorKit.upAction]
+            defaultAction.actionPerformed(null)
+          }
+      DOWN ->
+          if (isInHistoryMode) {
+            promptMessageHistory.popLowerMessage(textArea)
+          } else {
+            val defaultAction = textArea.actionMap[DefaultEditorKit.downAction]
+            defaultAction.actionPerformed(null)
+          }
     }
   }
 
@@ -138,6 +165,7 @@ class PromptPanel(project: Project, private val chatSession: ChatSession) : JLay
     // Reset text
     promptMessageHistory.messageSent(text)
     textArea.text = ""
+    isInHistoryMode = true
     selectedContextItems.clear()
 
     chatSession.sendMessage(text, cf)