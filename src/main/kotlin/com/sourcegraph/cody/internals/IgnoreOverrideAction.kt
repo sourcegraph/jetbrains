498a7a94aff20887a127c16151402241333010c0
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-04-26 01:30:59 +0900

    Send a policy.

--- src/main/kotlin/com/sourcegraph/cody/internals/IgnoreOverrideAction.kt
+++ src/main/kotlin/com/sourcegraph/cody/internals/IgnoreOverrideAction.kt
@@ -1,5 +1,7 @@
 package com.sourcegraph.cody.internals
 
+import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
@@ -8,12 +10,11 @@ import com.intellij.openapi.ui.ValidationInfo
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.dsl.builder.*
 import com.sourcegraph.cody.agent.CodyAgentService
-import com.sourcegraph.cody.agent.protocol.TestingIgnoreOverridePolicy
+import com.sourcegraph.cody.agent.protocol.IgnorePolicySpec
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonObject
-import org.jetbrains.uast.util.isInstanceOf
 import javax.swing.JComponent
 
 data object ignoreOverrideModel {
@@ -46,10 +47,9 @@ class IgnoreOverrideDialog(val project: Project) : DialogWrapper(project) {
           .bindText(ignoreOverrideModel::policy)
           .validation { textArea ->
             try {
-              println("${ignoreOverrideModel.policy}/${textArea.text}")
-              val element = Json.parseToJsonElement(textArea.text)
-              if (element is JsonObject) { null } else { ValidationInfo("Policy must be a JSON object", textArea) }
-            } catch (e: SerializationException) {
+              Gson().fromJson(textArea.text, IgnorePolicySpec::class.java);
+              null
+            } catch (e: JsonSyntaxException) {
               ValidationInfo("JSON error: ${e.message}", textArea)
             }
           }
@@ -62,7 +62,7 @@ class IgnoreOverrideDialog(val project: Project) : DialogWrapper(project) {
     CodyAgentService.withAgent(project) { agent ->
       agent.server.testingIgnoreOverridePolicy(
           if (ignoreOverrideModel.enabled) {
-            Json.parseToJsonElement(ignoreOverrideModel.policy).jsonObject
+            Gson().fromJson(ignoreOverrideModel.policy, IgnorePolicySpec::class.java);
           } else {
             null
           })
