65465975c83d14465fdd1dfcf0b5f0e1d3223b53
Steve Yegge <stevey@sourcegraph.com>
2024-06-04 13:02:29 -0700

    fixed a compile error

--- src/integrationTest/kotlin/com/sourcegraph/cody/edit/DocumentCodeTest.kt
+++ src/integrationTest/kotlin/com/sourcegraph/cody/edit/DocumentCodeTest.kt
@@ -19,13 +19,13 @@ import com.sourcegraph.cody.edit.widget.LensGroupFactory
 import com.sourcegraph.cody.edit.widget.LensLabel
 import com.sourcegraph.cody.edit.widget.LensSpinner
 import com.sourcegraph.config.ConfigUtil
+import org.mockito.Mockito.mock
 import java.io.File
 import java.nio.file.Paths
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
 import java.util.regex.Pattern
-import org.mockito.Mockito.mock
 
 class DocumentCodeTest : BasePlatformTestCase() {
   private val logger = Logger.getInstance(DocumentCodeTest::class.java)
@@ -275,6 +275,7 @@ class DocumentCodeTest : BasePlatformTestCase() {
     }
   }
 
+  // Returns a future that completes when the topic is published.
   private fun subscribeToTopic(
       topic: Topic<CodyInlineEditActionNotifier>,
   ): CompletableFuture<CodyInlineEditActionNotifier.Context?> {
