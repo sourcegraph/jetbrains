40c1c9871c928751558b652e4953d6a851996160
Steve Yegge <stevey@sourcegraph.com>
2024-06-06 23:19:39 -0700

    Implemented VS Code-like hotkey help on selections

--- src/main/kotlin/com/sourcegraph/cody/listeners/CodySelectionListener.kt
+++ src/main/kotlin/com/sourcegraph/cody/listeners/CodySelectionListener.kt
@@ -1,5 +1,6 @@
 package com.sourcegraph.cody.listeners
 
+import CodySelectionInlayManager
 import com.intellij.openapi.editor.event.SelectionEvent
 import com.intellij.openapi.editor.event.SelectionListener
 import com.intellij.openapi.project.Project
@@ -9,19 +10,21 @@ import com.sourcegraph.cody.autocomplete.CodyAutocompleteManager
 import com.sourcegraph.config.ConfigUtil
 
 class CodySelectionListener(val project: Project) : SelectionListener {
+  private val inlayManager = CodySelectionInlayManager()
 
   override fun selectionChanged(event: SelectionEvent) {
     if (!ConfigUtil.isCodyEnabled() || event.editor == null) {
       return
     }
-
-    ProtocolTextDocument.fromEditorWithRangeSelection(event.editor, event)?.let { textDocument ->
+    val editor = event.editor
+    ProtocolTextDocument.fromEditorWithRangeSelection(editor, event)?.let { textDocument ->
       EditorChangesBus.documentChanged(project, textDocument)
       CodyAgentService.withAgent(project) { agent ->
         agent.server.textDocumentDidChange(textDocument)
       }
     }
 
-    CodyAutocompleteManager.instance.clearAutocompleteSuggestions(event.editor)
+    CodyAutocompleteManager.instance.clearAutocompleteSuggestions(editor)
+    inlayManager.handleSelectionChanged(editor, event)
   }
 }
