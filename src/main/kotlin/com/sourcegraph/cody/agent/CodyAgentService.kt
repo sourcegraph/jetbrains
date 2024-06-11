99e9c095ba96a97c070389eecd7e24c85e681884
Mikołaj Kondratek <mik.kondratek@gmail.com>
2024-06-11 12:09:44 +0200

    Revert CodyConsole.kt

--- src/main/kotlin/com/sourcegraph/cody/agent/CodyAgentService.kt
+++ src/main/kotlin/com/sourcegraph/cody/agent/CodyAgentService.kt
@@ -12,6 +12,7 @@ import com.sourcegraph.cody.chat.AgentChatSessionService
 import com.sourcegraph.cody.config.CodyApplicationSettings
 import com.sourcegraph.cody.context.RemoteRepoSearcher
 import com.sourcegraph.cody.edit.FixupService
+import com.sourcegraph.cody.error.CodyConsole
 import com.sourcegraph.cody.ignore.IgnoreOracle
 import com.sourcegraph.cody.listeners.CodyFileEditorListener
 import com.sourcegraph.cody.statusbar.CodyStatusService
@@ -134,6 +135,10 @@ class CodyAgentService(private val project: Project) : Disposable {
 
       agent.client.onIgnoreDidChange = Consumer {
         IgnoreOracle.getInstance(project).onIgnoreDidChange()
+      }
+
+      agent.client.onDebugMessage = Consumer { message ->
+        CodyConsole.getInstance(project).addMessage(message)
       }
 
       if (!project.isDisposed) {
