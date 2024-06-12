f43d943eea7b7ca8a39789eb2567bc8e60786b40
Ólafur Páll Geirsson <olafurpg@gmail.com>
2024-06-12 11:57:47 +0200

    Build: add Gradle settings to auto-download missing JDK
    Previously, I was unable to import the JetBrains plugin codebase into
    IntelliJ on my Windows computer because I didn't have a compatible
    installation of Java 11. This diff configures Gradle to automatically
    download the JDK so that I can import the build into IntelliJ on Windows
    without a custom setup.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,10 @@
 rootProject.name = "Sourcegraph"
 
+plugins {
+
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+}
+
 include(":jetbrains-shared")
 
 project(":jetbrains-shared").projectDir = file("../jetbrains-shared")
@@ -7,3 +12,4 @@ project(":jetbrains-shared").projectDir = file("../jetbrains-shared")
 val isCiServer = System.getenv().containsKey("CI")
 
 buildCache { local { isEnabled = !isCiServer } }
+
