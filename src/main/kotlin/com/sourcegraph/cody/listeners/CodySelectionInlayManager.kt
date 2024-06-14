fba73a8768da46aa3d7b1c2ad888ad41cd7b4b11
Steve Yegge <stevey@sourcegraph.com>
2024-06-14 14:31:06 -0700

    spotless

--- src/main/kotlin/com/sourcegraph/cody/listeners/CodySelectionInlayManager.kt
+++ src/main/kotlin/com/sourcegraph/cody/listeners/CodySelectionInlayManager.kt
@@ -74,7 +74,8 @@ class CodySelectionInlayManager {
                     editor.colorsScheme.getColor(EditorColors.SELECTION_BACKGROUND_COLOR)?.darker()
                 g.color = backgroundColor
 
-                val arcSize = getFont().size // Larger number = arc is more round, smaller = more square
+                val arcSize =
+                    getFont().size // Larger number = arc is more round, smaller = more square
                 val x = targetRegion.x.toDouble()
                 val y = targetRegion.y.toDouble()
                 val width = targetRegion.width.toDouble()
