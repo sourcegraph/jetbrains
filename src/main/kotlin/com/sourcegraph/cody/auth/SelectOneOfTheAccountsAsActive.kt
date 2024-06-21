7e2ea6a3b7ec599724f1bd154e79fc317becd4fc
Mikołaj Kondratek <mik.kondratek@gmail.com>
2024-06-21 15:01:39 +0200

    Fixes

--- src/main/kotlin/com/sourcegraph/cody/auth/SelectOneOfTheAccountsAsActive.kt
+++ src/main/kotlin/com/sourcegraph/cody/auth/SelectOneOfTheAccountsAsActive.kt
@@ -1,7 +1,6 @@
 package com.sourcegraph.cody.auth
 
 import com.intellij.openapi.project.Project
-import com.sourcegraph.cody.CodyToolWindowContent
 import com.sourcegraph.cody.config.CodyAuthenticationManager
 import com.sourcegraph.cody.config.getFirstAccountOrNull
 import com.sourcegraph.cody.initialization.Activity
@@ -13,7 +12,6 @@ class SelectOneOfTheAccountsAsActive : Activity {
       val newActiveAccount =
           CodyAuthenticationManager.getInstance(project).getAccounts().getFirstAccountOrNull()
       CodyAuthenticationManager.getInstance(project).setActiveAccount(newActiveAccount)
-      CodyToolWindowContent.executeOnInstanceIfNotDisposed(project) { refreshPanelsVisibility() }
     }
   }
 }
