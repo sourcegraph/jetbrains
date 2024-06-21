dcbbb3b1619b22289de0570691efe724dab90132
Piotr Kukielka <pkukielka@virtuslab.com>
2024-06-21 11:48:58 +0200

    Add intellij version and product code to v2 telemetry

--- src/main/kotlin/com/sourcegraph/cody/telemetry/TelemetryV2.kt
+++ src/main/kotlin/com/sourcegraph/cody/telemetry/TelemetryV2.kt
@@ -1,5 +1,6 @@
 package com.sourcegraph.cody.telemetry
 
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.project.Project
 import com.sourcegraph.cody.agent.CodyAgentService
 import com.sourcegraph.cody.agent.protocol.TelemetryEvent
@@ -7,15 +8,41 @@ import com.sourcegraph.cody.agent.protocol.TelemetryEventParameters
 
 class TelemetryV2 {
   companion object {
+    private val intellijProductCodeMap =
+        mapOf(
+            "IU" to 1L, // IntelliJ IDEA Ultimate
+            "IC" to 2L, // IntelliJ IDEA Community
+            "IE" to 3L, // IntelliJ IDEA Educational
+            "PS" to 4L, // PhpStorm
+            "WS" to 5L, // WebStorm
+            "PY" to 6L, // PyCharm Professional
+            "PC" to 7L, // PyCharm Community
+            "PE" to 8L, // PyCharm Educational
+            "RM" to 9L, // RubyMine
+            "OC" to 10L, // AppCode
+            "CL" to 11L, // CLion
+            "GO" to 12L, // GoLand
+            "DB" to 13L, // DataGrip
+            "RD" to 14L, // Rider
+            "AI" to 15L, // Android Studio
+        )
+
     fun sendTelemetryEvent(
         project: Project,
         feature: String,
         action: String,
         parameters: TelemetryEventParameters? = null
     ) {
+      val build = ApplicationInfo.getInstance().build
+      val versionParameters =
+          mapOf(
+              "ideProductCode" to intellijProductCodeMap.getOrDefault(build.productCode, 0L),
+              "ideBaselineVersion" to build.baselineVersion.toLong())
+      val newParameters = parameters?.copy(metadata = parameters.metadata?.plus(versionParameters))
+
       CodyAgentService.withAgent(project) { agent ->
         agent.server.recordEvent(
-            TelemetryEvent(feature = "cody.$feature", action = action, parameters = parameters))
+            TelemetryEvent(feature = "cody.$feature", action = action, parameters = newParameters))
       }
     }
 
