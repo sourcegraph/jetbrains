24e83dffc74aac643a42491055c9600f2be6b4db
Steve Yegge <stevey@sourcegraph.com>
2024-07-02 09:43:44 -0700

    added more error handling when finding repo roots
    was failing during tests

--- src/main/kotlin/com/sourcegraph/cody/agent/CodyAgentCodebase.kt
+++ src/main/kotlin/com/sourcegraph/cody/agent/CodyAgentCodebase.kt
@@ -3,6 +3,7 @@ package com.sourcegraph.cody.agent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.sourcegraph.cody.config.CodyProjectSettings
@@ -13,7 +14,6 @@ import java.util.concurrent.CompletableFuture
 @Service(Service.Level.PROJECT)
 class CodyAgentCodebase(val project: Project) {
 
-  // TODO: Support list of repository names instead of just one.
   private val settings = CodyProjectSettings.getInstance(project)
   private var inferredUrl: CompletableFuture<String> = CompletableFuture()
 
@@ -29,17 +29,23 @@ class CodyAgentCodebase(val project: Project) {
     // This can happen during testing with certain temporary files.
     if (file == null) return
     ApplicationManager.getApplication().executeOnPooledThread {
-      val repositoryName = RepoUtil.findRepositoryName(project, file)
-      if (repositoryName != null && inferredUrl.getNow(null) != repositoryName) {
-        inferredUrl.complete(repositoryName)
-        CodyAgentService.withAgent(project) {
-          it.server.configurationDidChange(ConfigUtil.getAgentConfiguration(project))
+      try {
+        val repositoryName = RepoUtil.findRepositoryName(project, file)
+        if (repositoryName != null && inferredUrl.getNow(null) != repositoryName) {
+          inferredUrl.complete(repositoryName)
+          CodyAgentService.withAgent(project) {
+            it.server.configurationDidChange(ConfigUtil.getAgentConfiguration(project))
+          }
         }
+      } catch (x: Exception) {
+        logger.warn("Error finding repository name for $file", x)
       }
     }
   }
 
   companion object {
+    private val logger = Logger.getInstance(CodyAgentCodebase::class.java)
+
     @JvmStatic
     fun getInstance(project: Project): CodyAgentCodebase {
       return project.service()
