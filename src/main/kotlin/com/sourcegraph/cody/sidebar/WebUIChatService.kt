7163ee1b55b94cd2be93300b38e50b1771db172d
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-07-05 13:00:40 +0900

    More postMessage plumbing.

--- src/main/kotlin/com/sourcegraph/cody/sidebar/WebUIChatService.kt
+++
@@ -1,7 +0,0 @@
-package com.sourcegraph.cody.sidebar
-
-import com.intellij.openapi.components.Service
-import com.intellij.openapi.components.service
-import com.intellij.openapi.project.Project
-import com.sourcegraph.cody.agent.protocol.WebviewCreateWebviewPanelParams
-import com.sourcegraph.cody.ui.WebUIProxy
