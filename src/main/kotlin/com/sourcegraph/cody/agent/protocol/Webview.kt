6db78442fbdc4ed76b26cd3c7652606b69091392
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-07-08 15:44:28 +0900

    Hook up command execution, although cody.vscode.open fails because vscode.open is not found.

--- src/main/kotlin/com/sourcegraph/cody/agent/protocol/Webview.kt
+++ src/main/kotlin/com/sourcegraph/cody/agent/protocol/Webview.kt
@@ -1,5 +1,23 @@
 package com.sourcegraph.cody.agent.protocol
 
+data class WebviewCreateWebviewPanelPortMapping(
+  val webviewPort: Int,
+  val extensionHostPort: Int
+)
+
+data class WebviewCreateWebviewPanelOptions(
+  val enableScripts: Boolean,
+  val enableForms: Boolean,
+  // boolean | readonly string[]
+  val enableCommandUris: Any,
+  // Note, we model "missing" here because interpreting the default
+  // depends on the current workspace root.
+  val localResourceRoots: List<String>,
+  val portMapping: List<WebviewCreateWebviewPanelPortMapping>,
+  val enableFindWidget: Boolean,
+  val retainContextWhenHidden: Boolean,
+)
+
 data class WebviewCreateWebviewPanelShowOptions(
     val preserveFocus: Boolean,
     val viewColumn: Int,
@@ -10,4 +28,5 @@ data class WebviewCreateWebviewPanelParams(
   val viewType: String,
   val title: String,
   val showOptions: WebviewCreateWebviewPanelShowOptions,
+  val options: WebviewCreateWebviewPanelOptions
 )
