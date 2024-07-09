32015a4e2a51c3d305f3b0dbfd2a106285ee466f
Steve Yegge <stevey@sourcegraph.com>
2024-07-09 12:55:15 -0700

    ran spotless

--- src/main/kotlin/com/sourcegraph/config/ConfigUtil.kt
+++ src/main/kotlin/com/sourcegraph/config/ConfigUtil.kt
@@ -16,10 +16,10 @@ import com.sourcegraph.cody.config.CodyAuthenticationManager
 import com.sourcegraph.cody.config.ServerAuthLoader
 import com.sourcegraph.cody.config.SourcegraphServerPath
 import com.sourcegraph.cody.config.SourcegraphServerPath.Companion.from
-import org.jetbrains.annotations.Contract
-import org.jetbrains.annotations.VisibleForTesting
 import java.nio.file.Path
 import java.nio.file.Paths
+import org.jetbrains.annotations.Contract
+import org.jetbrains.annotations.VisibleForTesting
 
 object ConfigUtil {
   const val DOTCOM_URL = "https://sourcegraph.com/"
@@ -131,7 +131,8 @@ object ConfigUtil {
 
   @JvmStatic fun isCodyDebugEnabled(): Boolean = CodyApplicationSettings.instance.isCodyDebugEnabled
 
-  @JvmStatic fun isCodyUIHintsEnabled(): Boolean = CodyApplicationSettings.instance.isCodyUIHintsEnabled
+  @JvmStatic
+  fun isCodyUIHintsEnabled(): Boolean = CodyApplicationSettings.instance.isCodyUIHintsEnabled
 
   @JvmStatic
   fun isCodyVerboseDebugEnabled(): Boolean =
