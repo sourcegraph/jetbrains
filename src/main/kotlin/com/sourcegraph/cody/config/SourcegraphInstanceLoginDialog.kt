4a9d197d9470f056d8c1a2c9e517e21e65d1095b
Mikołaj Kondratek <mik.kondratek@gmail.com>
2024-07-12 14:54:31 +0200

    New auth UI - add Advanced...

--- src/main/kotlin/com/sourcegraph/cody/config/SourcegraphInstanceLoginDialog.kt
+++ src/main/kotlin/com/sourcegraph/cody/config/SourcegraphInstanceLoginDialog.kt
@@ -12,16 +12,21 @@ import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.util.ProgressIndicatorUtils
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.ValidationInfo
+import com.intellij.openapi.ui.setEmptyState
 import com.intellij.openapi.util.Disposer
 import com.intellij.ui.AnimatedIcon
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBTextField
+import com.intellij.ui.components.fields.ExtendableTextField
+import com.intellij.ui.dsl.builder.MAX_LINE_LENGTH_NO_WRAP
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.gridLayout.HorizontalAlign
 import com.intellij.ui.layout.not
 import com.intellij.ui.layout.selected
+import com.intellij.ui.layout.selectedValueMatches
 import com.intellij.util.ui.UIUtil.getInactiveTextColor
 import com.sourcegraph.cody.api.SourcegraphApiRequestExecutor
 import com.sourcegraph.cody.api.SourcegraphAuthenticationException
@@ -37,8 +42,12 @@ class SourcegraphInstanceLoginDialog(project: Project?, parent: Component?) :
 
   private var tokenAcquisitionError: ValidationInfo? = null
   private lateinit var instanceUrlField: JBTextField
+  private lateinit var tokenField: JBTextField
+  private lateinit var customRequestHeadersField: ExtendableTextField
   internal lateinit var codyAuthData: CodyAuthData
+
   private val isAcquiringToken = JCheckBox().also { it.isSelected = false }
+  private lateinit var advancedSettings: ComboBox<String>
 
   init {
     title = "Add Sourcegraph Account"
@@ -46,30 +55,61 @@ class SourcegraphInstanceLoginDialog(project: Project?, parent: Component?) :
     init()
   }
 
-  override fun createCenterPanel() =
-      panel {
-      row {
-            cell(
-                JBLabel("Logging in, check your browser").apply {
-                  icon = AnimatedIcon.Default.INSTANCE
-                  foreground = getInactiveTextColor()
-                })
+  override fun createCenterPanel() = panel {
+    row {
+          cell(
+              JBLabel("Logging in, check your browser").apply {
+                icon = AnimatedIcon.Default.INSTANCE
+                foreground = getInactiveTextColor()
+              })
+        }
+        .visibleIf(isAcquiringToken.selected)
+    row("Instance URL:") {
+          textField()
+              .applyToComponent {
+                emptyText.text = "https://sourcegraph.yourcompany.com"
+                text = "sourcegraph.com" // todo: remove me
+                instanceUrlField = this
+              }
+              .horizontalAlign(HorizontalAlign.FILL)
+        }
+        .rowComment(
+            "Enter the address of your sourcegraph instance. For example https://sourcegraph.example.org",
+            maxLineLength = MAX_LINE_LENGTH_NO_WRAP)
+        .visibleIf(isAcquiringToken.selected.not())
+    row("Advanced settings:") {
+      comboBox(listOf("Browser", "Token")).applyToComponent {
+        advancedSettings = this
+        advancedSettings.addActionListener {
+          if (advancedSettings.selectedItem == "Token") {
+            setOKButtonText("Add Account")
+          } else {
+            setOKButtonText("Authorize in browser")
           }
-          .visibleIf(isAcquiringToken.selected)
-      row("Instance URL:") {
-            textField()
-                .applyToComponent {
-                  emptyText.text = "https://sourcegraph.yourcompany.com"
-                  text = "sourcegraph.com" // todo: remove me
-                  instanceUrlField = this
-                }
-                .horizontalAlign(HorizontalAlign.FILL)
-          }
-          .rowComment(
-              "Enter the address of your sourcegraph instance. For example https://sourcegraph.example.org")
-          .visibleIf(isAcquiringToken.selected.not())
+        }
+      }
     }
-
+    row("Token:") {
+          textField().applyToComponent { tokenField = this }.horizontalAlign(HorizontalAlign.FILL)
+        }
+        .enabledIf(advancedSettings.selectedValueMatches("Token"::equals))
+    group("Advanced settings", indent = false) {
+          row("Custom request headers: ") {
+            customRequestHeadersField = ExtendableTextField("", 0)
+            cell(customRequestHeadersField)
+                .horizontalAlign(HorizontalAlign.FILL)
+                .comment(
+                    """Any custom headers to send with every request to Sourcegraph.<br>
+                  |Use any number of pairs: "header1, value1, header2, value2, ...".<br>
+                  |Whitespace around commas doesn't matter.
+              """
+                        .trimMargin(),
+                    MAX_LINE_LENGTH_NO_WRAP)
+                .applyToComponent { setEmptyState("Client-ID, client-one, X-Extra, some metadata") }
+          }
+        }
+        .enabledIf(advancedSettings.selectedValueMatches("Token"::equals))
+  }
 
   override fun createActions(): Array<Action> = arrayOf(cancelAction, okAction)
 
