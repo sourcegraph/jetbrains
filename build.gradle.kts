c272b9ebbbdd70e3d71149b28e4a5b3f8ca5b85d
Mikołaj Kondratek <mik.kondratek@gmail.com>
2024-08-06 11:34:59 +0200

    Do not fix plugin verifier version

--- build.gradle.kts
+++ build.gradle.kts
@@ -519,7 +519,6 @@ tasks {
   runPluginVerifier {
     ideVersions.set(versionsToValidate)
     failureLevel.set(EnumSet.complementOf(skippedFailureLevels))
-    verifierVersion.set("1.371")
   }
 
   // Configure UI tests plugin
