8ea358248bb936a005e13b3a6613ba42deb83cb6
Mikołaj Kondratek <mik.kondratek@gmail.com>
2024-08-28 13:46:07 +0200

    Fixes

--- src/main/kotlin/com/sourcegraph/utils/CodyFormatter.kt
+++ src/main/kotlin/com/sourcegraph/utils/CodyFormatter.kt
@@ -42,14 +42,14 @@ class CodyFormatter {
         // Fix for the IJ formatting bug which removes spaces even before the given formatting
         // range.
         val existingStart = appendedString.substring(0, cursor)
-        val endIndexInPsiFile = min(cursor, psiFile.text.length)
-        val formattedStart = psiFile.text.substring(0, endIndexInPsiFile)
+        val boundedCursorPosition = min(cursor, psiFile.text.length)
+        val formattedStart = psiFile.text.substring(0, boundedCursorPosition)
         val startOfDiff = existingStart.zip(formattedStart).indexOfFirst { (e, f) -> e != f }
 
         val formattedText =
             if (startOfDiff != -1) {
               val addition = formattedStart.substring(startOfDiff)
-              existingStart + addition + psiFile.text.substring(endIndexInPsiFile)
+              existingStart + addition + psiFile.text.substring(boundedCursorPosition)
             } else psiFile.text
         return formattedText.substring(
             range.startOffset, range.endOffset + formattedText.length - document.textLength)
