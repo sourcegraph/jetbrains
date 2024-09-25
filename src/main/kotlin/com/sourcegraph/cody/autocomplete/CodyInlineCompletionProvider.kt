3cd356d8c7a5989eb148f6f1c0c9f16c6f4a05c1
Mikołaj Kondratek <mik.kondratek@gmail.com>
2024-09-25 12:57:33 +0200

    Remove redundant files

--- src/main/kotlin/com/sourcegraph/cody/autocomplete/CodyInlineCompletionProvider.kt
+++ src/main/kotlin/com/sourcegraph/cody/autocomplete/CodyInlineCompletionProvider.kt
@@ -1,8 +1,8 @@
 package com.sourcegraph.cody.autocomplete
 
+import com.intellij.codeInsight.inline.completion.InlineCompletionElement
 import com.intellij.codeInsight.inline.completion.InlineCompletionEvent
 import com.intellij.codeInsight.inline.completion.InlineCompletionProvider
-import com.intellij.codeInsight.inline.completion.InlineCompletionProviderID
 import com.intellij.codeInsight.inline.completion.InlineCompletionRequest
 import com.intellij.codeInsight.inline.completion.InlineCompletionSuggestion
 import com.intellij.codeInsight.inline.completion.elements.InlineCompletionGrayTextElement
@@ -10,6 +10,7 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
@@ -47,12 +48,15 @@ import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicReference
 import org.eclipse.lsp4j.jsonrpc.ResponseErrorException
 
+@JvmInline
+value class InlineCompletionProviderID(val id: String)
+
 class CodyInlineCompletionProvider : InlineCompletionProvider {
   private val logger = Logger.getInstance(CodyInlineCompletionProvider::class.java)
   private val currentJob = AtomicReference(CancellationToken())
-  override val id = InlineCompletionProviderID("Cody")
+  val id = InlineCompletionProviderID("Cody")
 
-  override suspend fun getSuggestion(request: InlineCompletionRequest): InlineCompletionSuggestion {
+  suspend fun getSuggestion(request: InlineCompletionRequest): InlineCompletionSuggestion {
     val editor = request.editor
     val project = editor.project ?: return InlineCompletionSuggestion.empty()
     val enabled = isImplicitAutocompleteEnabledForEditor(editor)
@@ -213,7 +217,15 @@ class CodyInlineCompletionProvider : InlineCompletionProvider {
     return 0
   }
 
-  override fun isEnabled(event: InlineCompletionEvent): Boolean {
+  fun isEnabled(event: InlineCompletionEvent): Boolean {
     return ConfigUtil.isCodyEnabled() && ConfigUtil.isCodyAutocompleteEnabled()
+  }
+
+  override suspend fun getProposals(request: InlineCompletionRequest): List<InlineCompletionElement> {
+    TODO("Not yet implemented")
+  }
+
+  override fun isEnabled(event: DocumentEvent): Boolean {
+    TODO("Not yet implemented")
   }
 }
