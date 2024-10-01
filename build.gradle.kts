18128c9a82e814858f621d634086c26bd8d4156f
Mikołaj Kondratek <mik.kondratek@gmail.com>
2024-10-01 13:20:35 +0200

    Do not skip COMPATIBILITY_PROBLEMS in verification

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,6 @@ val versionsToValidate =
     }
 val skippedFailureLevels =
     EnumSet.of(
-        FailureLevel.COMPATIBILITY_PROBLEMS, // blocked by: compatibility hack for IJ 2022.1 / 2024+
         FailureLevel.DEPRECATED_API_USAGES,
         FailureLevel.INTERNAL_API_USAGES,
         FailureLevel.SCHEDULED_FOR_REMOVAL_API_USAGES, // blocked by: Kotlin UI DSL Cell.align
