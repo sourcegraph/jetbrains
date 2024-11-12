5365b0ba2f3069075d5c3f9820c134f428f04bb3
Mikołaj Kondratek <mik.kondratek@gmail.com>
2024-11-12 22:48:14 +0100

    Migrate ignore/test

--- src/main/kotlin/com/sourcegraph/cody/agent/protocol/Ignore.kt
+++ src/main/kotlin/com/sourcegraph/cody/agent/protocol/Ignore.kt
@@ -1,11 +1,5 @@
 package com.sourcegraph.cody.agent.protocol
 
-data class IgnoreTestParams(val uri: String)
-
-data class IgnoreTestResponse(
-    val policy: String // "use" or "ignore"
-)
-
 data class IgnorePolicyPattern(val repoNamePattern: String, val filePathPatterns: List<String>?)
 
 data class IgnorePolicySpec(
